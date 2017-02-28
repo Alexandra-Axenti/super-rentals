45ce0979ececaf445a78ff5bb5e47b64eb26521a
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-02-28 16:39:28 +0800

    added announcement route and announcement-tile component

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      announcements: this.store.findAll('announcement')
+    });
   },
 
   actions: {
