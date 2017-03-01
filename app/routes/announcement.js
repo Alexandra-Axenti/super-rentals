0c04a7398661d45b04c6f99b86e6f1c91377c9fd
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-01 11:04:52 +0800

    add the final work on the update feature - had bugs in having rental wording in the announcement components

--- app/routes/announcement.js
+++ app/routes/announcement.js
@@ -6,7 +6,18 @@ export default Ember.Route.extend({
       announcements: this.store.findAll('announcement')
     });
   },
+
   actions: {
+    update(announcement, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          announcement.set(key,params[key]);
+        }
+      });
+      announcement.save();
+      this.transitionTo('announcement');
+    },
+
     saveAnnouncement(params) {
           var newAnnouncement = this.store.createRecord('announcement', params);
           newAnnouncement.save();
